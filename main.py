9a2c6c165500b874306adb75a231751ee002e5ca
ahsan_admin <ahsan.nayaz@blenheimchalcot.com>
2024-01-16 12:06:14 +0000

    Database update

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import os
 
 from langchain.chat_models import AzureChatOpenAI
+from langchain_openai import ChatOpenAI
+from langchain.chains.openai_functions import create_openai_fn_runnable
+from langchain_core.pydantic_v1 import BaseModel, Field, UUID4
 from langchain.schema import StrOutputParser
 from langchain.schema.runnable import Runnable
 from langchain.schema.runnable.config import RunnableConfig
@@ -9,12 +12,19 @@ from langchain_community.chat_message_histories import RedisChatMessageHistory
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.runnables.history import RunnableWithMessageHistory
 import uuid
+from datetime import datetime
 import chainlit as cl
 from dotenv import load_dotenv
+import psycopg2
 
 load_dotenv(dotenv_path='venv/.env')
 
 REDIS_URL = os.getenv('REDIS_URL')
+PGHOST = os.getenv('PGHOST')
+PGUSER = os.getenv('PGUSER')
+PGPORT = os.getenv('PGPORT')
+PGDATABASE = os.getenv('PGDATABASE')
+PGPASSWORD = os.getenv('PGPASSWORD')
 
 
 @cl.oauth_callback
@@ -26,6 +36,9 @@ def oauth_callback(
 ) -> Optional[cl.User]:
     return default_user
 
+CHAT_INFO_PROMPT = '''
+You are a Social-Care Chat Transcript Analyser. You are to analyse a given transcript and provide the details in the given format. 
+'''
 
 PROMPT = '''
 You are a Social Care chatbot for the Wigan Council in UK. You answer all the queries of the user and follow the following flow of actions if the user asks for social care support:
@@ -146,14 +159,20 @@ async def on_message(message: cl.Message):
 
             await msg.send()
         except:
-            if "'self_harm': {'filtered': True, 'severity': 'medium'}" or "'self_harm': {'filtered': True, 'severity': 'high'}" in message.content:
+            if "'self_harm': {'filtered': True, 'severity': 'medium'}" or \
+                    "'self_harm': {'filtered': True, 'severity': 'high'}" in message.content:
                 await cl.Message(
-                    content="It looks like your message mentions self-harm. If you, or someone you know, is at risk or is experiencing self-harm, please contact the emergency services immediately.").send()
-            elif "'violence': {'filtered': True, 'severity': 'medium'}" or "'violence': {'filtered': True, 'severity': 'high'}" in message.content:
+                    content="It looks like your message mentions self-harm. If you, or someone you know, is at risk or "
+                            "is experiencing self-harm, please contact the emergency services immediately.").send()
+            elif "'violence': {'filtered': True, 'severity': 'medium'}" or \
+                    "'violence': {'filtered': True, 'severity': 'high'}" in message.content:
                 await cl.Message(
-                    content="It looks like your message mentions violence. If you, or someone you know, is at risk or is experiencing violence, please contact the emergency services immediately.").send()
+                    content="It looks like your message mentions violence. If you, or someone you know, is at risk or "
+                            "is experiencing violence, please contact the emergency services immediately.").send()
             else:
-                await cl.Message(content="I'm sorry, but your message has been flagged as containing harmful content by our content moderation policy. Please re-write your message and try again.").send()
+                await cl.Message(
+                    content="I'm sorry, but your message has been flagged as containing harmful content by our content "
+                            "moderation policy. Please re-write your message and try again.").send()
 
     cl.user_session.set('end', end)
     cl.user_session.set('transcript', transcript)
@@ -161,7 +180,6 @@ async def on_message(message: cl.Message):
 
 @cl.action_callback("End chat")
 async def on_action_end(action: cl.Action):
-    cl.user_session.set("id", str(uuid.uuid4()))
     await cl.Message(content="Chat ended!").send()
     await action.remove()
 
@@ -176,4 +194,72 @@ async def on_action_transcript(action: cl.Action):
 
 @cl.on_chat_end
 async def on_chat_end():
+    conn = await init_db()
+    cursor = conn.cursor()
+    insert_query, values = await chat_records()
+    cursor.execute(insert_query, values)
+    conn.commit()
+    cursor.close()
+    conn.close()
     await cl.Message(content="Chat processed and saved!").send()
+
+
+@cl.cache
+async def init_db():
+    cnx = psycopg2.connect(user=PGUSER, password=PGPASSWORD, host=PGHOST, port=PGPORT, database=PGDATABASE)
+    return cnx
+
+
+class ChatInfo(BaseModel):
+    """Represents a chat record."""
+
+    chat_summary: str = Field(..., description="Summary of the chat")
+    category: str = Field(..., description="Category of the chat (EIP/SC/Urgent Needs/General Queries)")
+    severity: str = Field(..., description="Severity of the issue highlighted by the user")
+    social_care_eligibility: Optional[str] = Field(None, description="Eligibility for social care (if applicable else NaN)")
+    suggested_course_of_action: str = Field(..., description="Suggested course of action from the transcript")
+    next_steps: str = Field(..., description="Suggest next steps to the assessor")
+    contact_request: str = Field(..., description="Whether the user has requested for Contact (Yes/No)")
+    status: str = Field(..., description="Status of the chat (Completed/Incomplete)")
+
+
+async def chat_records():
+    session_id = cl.user_session.get('id')
+    name = cl.user_session.get('user')
+    email_or_phone_number = cl.user_session.get('user')
+    datetime_of_chat = datetime.now()
+    chat_duration = 30
+    chat_transcript = cl.user_session.get("runnable").get_session_history(session_id)
+    chat_info = await get_chat_info(session_id)
+    insert_query = """
+        INSERT INTO ChatRecords (
+            SessionID, Name, EmailOrPhoneNumber, DateTimeOfChat, ChatDuration, ChatTranscript,
+            ChatSummary, Category, Severity, SocialCareEligibility, SuggestedCourseOfAction,
+            NextSteps, ContactRequest, Status
+        ) VALUES (
+            %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s
+        );
+    """
+
+    values = (
+        session_id, name, email_or_phone_number, datetime_of_chat, chat_duration, chat_transcript,
+        chat_info.chat_summary, chat_info.category, chat_info.severity, chat_info.social_care_eligibility,
+        chat_info.suggested_course_of_action, chat_info.next_steps, chat_info.contact_request, chat_info.status
+    )
+
+    return insert_query, values
+
+
+async def get_chat_info(session_id):
+    llm = ChatOpenAI(model="gpt-3.5-turbo-1106")
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", CHAT_INFO_PROMPT),
+            ("human", "Make calls to the relevant function to record the entities in the following transcript: {input}"),
+            ("human", "Tip: Make sure to answer in the correct format"),
+        ]
+    )
+    transcript = cl.user_session.get("runnable").get_session_history(session_id)
+    chain = create_openai_fn_runnable([ChatInfo], llm, prompt)
+    return chain.invoke({"input": transcript})
+
