893bea4c124070dbc732f08a93c5e6d3ed974a77
ramchandrapatilBCI <149769799+ramchandrapatilBCI@users.noreply.github.com>
2024-02-16 11:55:40 +0530

    changed import statement

--- Tests/test_UITestContextRelevency_test.py
+++ Tests/test_UITestContextRelevency_test.py
@@ -1,5 +1,5 @@
 import time
-# from telnetlib import EC
+from telnetlib import EC
 import pytest
 from deepeval.metrics import ContextualRelevancyMetric
 from deepeval.test_case import LLMTestCase
@@ -7,7 +7,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+# from selenium.webdriver.support import expected_conditions as EC
 from deepeval.metrics import ToxicityMetric
 import telnetlib3
 from deepeval.metrics.ragas import CorrectnessMetric
@@ -18,6 +18,11 @@ options = webdriver.ChromeOptions()
 @pytest.fixture(params=["chrome"])
 def driver_init(request):
     if request.param == "chrome":
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option("excludeSwitches", ["enable-automation"])
+        options.add_argument('headless')
+        prefs = {"credentials_enable_service": False, "profile.password_manager_enabled": False}
+        options.add_experimental_option("prefs", prefs)
         web_driver = webdriver.Chrome(options)
     if request.param == "firefox":
         web_driver = webdriver.Firefox()
