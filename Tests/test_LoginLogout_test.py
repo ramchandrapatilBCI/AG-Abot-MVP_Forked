d1de1189441f008e3f77b6a79117c4633f3d3355
ramchandrapatilBCI <149769799+ramchandrapatilBCI@users.noreply.github.com>
2024-02-20 13:17:39 +0530

    Added chrome options

--- Tests/test_LoginLogout_test.py
+++ Tests/test_LoginLogout_test.py
@@ -11,7 +11,8 @@ class TestLogin:
     def driver(self):
         options = webdriver.ChromeOptions()
         options.add_experimental_option("excludeSwitches", ["enable-automation"])
-        options.add_argument('headless')
+        options.add_argument("--no-sandbox")
+        options.add_argument("--headless")
         options.add_argument("--disable-dev-shm-usage")
         prefs = {"credentials_enable_service": False, "profile.password_manager_enabled": False}
         options.add_experimental_option("prefs", prefs)
